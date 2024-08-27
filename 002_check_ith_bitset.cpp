f5017545618136c4121568f5f214979aad2e7706
Muthuvel <muthuveldiary@gmail.com>
2024-08-27 19:30:28 +0530

    intial commit

---
+++ 002_check_ith_bitset.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    if(n&(k>>1)) cout<<"BIT IS SET";
+    else cout<<"BIT IS NOTSET";
+    
+
+    return 0;   
+}
